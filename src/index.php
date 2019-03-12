ce370a9574eb97716b19cad528bd29fef9980f24
it <it@it04>
2019-03-12 18:24:12 +0700

    Initial

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Orr\HelloWorld;
+
+/**
+ * Description of index
+ *
+ * @author it
+ */
+class Index {
+
+    public function greet($greet = "Hello World") {
+        return $greet;
+    }
+
+}
